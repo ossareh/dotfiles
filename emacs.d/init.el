dd05020228e4913ada2d56535477407c179a720e
P. Michael Ossareh <ossareh@gmail.com>
2015-11-27 11:53:15 -0800

    fix lockscreen and commit laptoponly

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,6 +4,7 @@
 (defvar repos '(("marmalade" . "http://marmalade-repo.org/packages/")
                 ("melpa" . "http://melpa.milkbox.net/packages/")
                 ("gnu" . "http://elpa.gnu.org/packages/")))
+
 (dolist (r repos)
   (add-to-list 'package-archives r t))
 (package-initialize)

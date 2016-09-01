a6ebbe26281ff70bdcd9be95eaef28d5d6f2dd0b
Michael Ossareh <ossareh@users.noreply.github.com>
2016-08-31 19:57:46 -0500

    Create git.sh

---
+++ profile.d/git.sh
@@ -0,0 +1,37 @@
+COLOR_RED="\033[0;31m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_GREEN="\033[0;32m"
+COLOR_OCHRE="\033[38;5;95m"
+COLOR_BLUE="\033[0;34m"
+COLOR_WHITE="\033[0;37m"
+COLOR_RESET="\033[0m"
+
+function git_color {
+  local git_status="$(git status 2> /dev/null)"
+
+  if [[ ! $git_status =~ "working directory clean" ]]; then
+    echo -e $COLOR_YELLOW
+  else
+    echo -e $COLOR_GREEN
+  fi
+}
+
+function git_branch {
+  local git_status="$(git status 2> /dev/null)"
+  local on_branch="On branch ([^${IFS}]*)"
+  local on_commit="HEAD detached at ([^${IFS}]*)"
+
+  if [[ $git_status =~ $on_branch ]]; then
+    local branch=${BASH_REMATCH[1]}
+    echo "($branch)"
+  elif [[ $git_status =~ $on_commit ]]; then
+    local commit=${BASH_REMATCH[1]}
+    echo "($commit)"
+  fi
+}
+
+PS1="\n\[$COLOR_WHITE\][\W]"  # basename of pwd
+PS1+="\[\$(git_color)\]"    # colors git status
+PS1+="\$(git_branch)"       # prints current branch
+PS1+="\[$COLOR_RESET\]\$ "  # '#' for root, else '$'
+export PS1

2f4581d4ad292f7e03d394e56797f4f24e19c200
P. Michael Ossareh <ossareh@gmail.com>
2025-01-19 12:43:30 -0600

    macos: configuration

--- modules/base/macos.nix
+++ modules/base/macos.nix
@@ -1,4 +1,7 @@
 {inputs, ...}: {
+  # all my macos are apple silicon
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
   # Set Git commit hash for darwin-version.
   system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
 
@@ -6,5 +9,56 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
 
-  nix.settings.trusted-users = ["@admin"];
+  # sanity
+  system = {
+    defaults = {
+      dock = {
+        autohide = false;
+        largesize = 96;
+        magnification = true;
+        tilesize = 48;
+        minimize-to-application = true;
+        wvous-tr-corner = 2;
+        wvous-tl-corner = 5;
+      };
+
+      finder = {
+        FXPreferredViewStyle = "clmv";
+        ShowPathbar = true;
+        ShowStatusBar = true;
+      };
+
+      loginwindow.SHOWFULLNAME = true;
+
+      menuExtraClock = {
+        Show24Hour = true;
+        ShowDate = 0;
+      };
+
+      screensaver = {
+        askForPassword = true;
+        askForPasswordDelay = 5;
+      };
+
+      NSGlobalDomain = {
+        AppleShowAllFiles = true;
+        InitialKeyRepeat = 11;
+        KeyRepeat = 4;
+        # "Natural" Scrolling
+        "com.apple.swipescrolldirection" = false;
+      };
+    };
+
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+  };
+
+  power = {
+    sleep = {
+      display = 5;
+      computer = 10;
+    };
+  };
 }
--- modules/base/system.nix
+++ modules/base/system.nix
@@ -1,4 +1,7 @@
 {
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
+
+  # add all admins to the trusted user set
+  nix.settings.trusted-users = ["@admin"];
 }

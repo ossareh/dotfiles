1f16dc7af4ad42223e869724a4ffc82088ebec3d
P. Michael Ossareh <ossareh@gmail.com>
2025-06-17 20:02:35 -0500

    feat: port over nix.settings & default packages

--- systems/aarch64-darwin/plinth/default.nix
+++ systems/aarch64-darwin/plinth/default.nix
@@ -1,6 +1,32 @@
-{...}: {
+{pkgs, ...}: {
   system.stateVersion = 6;
   system.primaryUser = "ossareh";
+
+  nix.settings = {
+    cores = 8;
+    max-jobs = 8;
+
+    experimental-features = "nix-command flakes";
+
+    trusted-users = ["@admin"];
+
+    # use caches
+    substituters = [
+      "https://cache.nixos.org"
+      "https://cache.nixos.org/"
+      "https://ossareh.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "ossareh.cachix.org-1:qk9AlH0oG2y7Cw2TonUjE6Ji8RnLNUSrU+nFhIYQ8IM="
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    cachix
+    curl
+  ];
 
   homebrew = {
     enable = true;

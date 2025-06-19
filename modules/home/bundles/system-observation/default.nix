cc604cf3901b6cc073ddd2cec83367c10463ed06
P. Michael Ossareh <ossareh@gmail.com>
2025-06-18 20:35:58 -0500

    feat: implement bundles concept (#33)

---
+++ modules/home/bundles/system-observation/default.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  namespace,
+  lib,
+  ...
+}: let
+  inherit (lib.${namespace}) enabled;
+
+  cfg = config.${namespace}.bundles.system-observation;
+in {
+  options.${namespace}.bundles.system-observation = {
+    enable = lib.mkEnableOption "system-observation";
+  };
+
+  config = lib.mkIf cfg.enable {
+    ossarehnix = {
+      programs = {
+        btop = enabled;
+      };
+
+      tools = {
+        dogdns = enabled;
+        duf = enabled;
+        dust = enabled;
+        procs = enabled;
+      };
+    };
+  };
+}
